61d8fba4dadf8877bd3c2c681b8fd5c54ee7dcf4
Raphabal <46353401+Raphabal@users.noreply.github.com>
2024-01-27 19:50:06 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -18,7 +18,7 @@ import pandas as pd
 
 LOGGER = get_logger(__name__)
 st.write("# MVP Petroleo! :oil_drum: ")
-petroleo_base = pd.read_csv("https://raw.githubusercontent.com/Raphabal/techchallenge04deploy/main/BDBase.csv", sep=';')
+petroleo_base = pd.read_csv("https://raw.githubusercontent.com/Raphabal/techchallenge04deploy/main/BDBase.csv", sep=';' encoding="latin-1", decimal=',', thousands=".", dayfirst=True))
 previsao_base = pd.read_csv("https://raw.githubusercontent.com/Raphabal/techchallenge04deploy/main/base_predicao.csv", sep=';')
 
 st.write("## Grafico de preços do petroleo ao longo dos anos! :chart_with_upwards_trend:")
